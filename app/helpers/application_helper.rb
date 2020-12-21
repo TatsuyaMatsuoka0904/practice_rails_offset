bfefc45b5ce36a5aab31ecfb437a334331631d9f
TatsuyaMatsuoka0904 <73767739+TatsuyaMatsuoka0904@users.noreply.github.com>
2020-12-21 09:52:22 +0900

    helperメソッド記述

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  #ERBテンプレ（レイアウトや部分テンプレを含む）とこで使用できるメソッドをヘルパーメソッドと言う
+  # app/helpersディレクトリトリにあるapplicationで定義してればできる
+  
+  def document_title
+    if @title.present?
+      "#{@title} - Baukis2"
+    else
+      'Baukis2'
+    end
+  end
 end
+
+#present?は blank?メソッドの否定で、
+# 長さが０、全て文字が空白である場合trueで返す。
+# nil.blank?は常にtrue
+#
